2480081fda11365f813daf6496ed7d87ade3532f
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2025-11-03 23:43:26 +0100

    fixed the bug that raised the error

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,11 @@
-x = eval(input("Enter your monthly income: "))
-y = eval(input("Enter your total monthly expenses: "))
+x = int(input("Enter your monthly income: "))
+y = int(input("Enter your total monthly expenses: "))
 
 monthly_savings = x - y
 
 rate = 0.05
 
-projected_savings = monthly_savings * 12 + monthly_savings *12*rate
+projected_savings = monthly_savings * 12 + monthly_savings *12*0.05
 
 print(f"Your monthly savings are ${monthly_savings}")
 print(f"Projected savings after one year, with interest, is: ${projected_savings}")
