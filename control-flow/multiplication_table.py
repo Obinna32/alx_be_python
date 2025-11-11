95f387cb51f0d06e4e3496103c5382ddcfffdc69
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2025-11-12 00:41:24 +0100

    tried fixing the bug in ques2

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,4 @@
 number = eval(input("Enter a number to see its multiplication table: "))
 
-for i in range(10):
-    print(f"{number} * {i+1} =", number*(i+1))
-    i+=1
+for i in range(1,11):
+    print(f"{number} * {i} =", number*(i))
