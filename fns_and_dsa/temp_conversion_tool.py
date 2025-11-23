9d670af6e95c742dc150b702e81d268b626c538c
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2025-11-23 19:18:43 +0100

    last trial

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,7 +5,8 @@ def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    return (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
+    return CELSIUS_TO_FAHRENHEIT_FACTOR * celsius + 32
+
 
 def is_float(s):
     try:
