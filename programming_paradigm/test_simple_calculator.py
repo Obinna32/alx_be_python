c255422cac27f341c4fc7ee95cb3a1972df34ebc
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2025-11-30 22:12:11 +0100

    omoooo

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,68 +4,59 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        """Create a SimpleCalculator object before each test."""
+        """Set up a SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    #Addition Test
-    def test_addition_basic(self):
+    #  Addition
+    def test_addition(self):
+        """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-5, -3), -8)
-
-    def test_addition_with_zero(self):
-        self.assertEqual(self.calc.add(0, 5), 5)
-        self.assertEqual(self.calc.add(7, 0), 7)
-
-    def test_addition_large_numbers(self):
+        self.assertEqual(self.calc.add(0, 7), 7)
         self.assertEqual(self.calc.add(10**6, 10**6), 2_000_000)
 
-    #  Subtraction Test
-    def test_subtraction_basic(self):
+    #subtraction
+    def test_subtract(self):
+        """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(-5, -3), -2)
-
-    def test_subtraction_with_zero(self):
         self.assertEqual(self.calc.subtract(7, 0), 7)
         self.assertEqual(self.calc.subtract(0, 7), -7)
 
-    def test_subtraction_large_numbers(self):
-        self.assertEqual(self.calc.subtract(10**6, 10**5), 900_000)
-
-    # Multiplication Tests
-    def test_multiply_basic(self):
+    #Multplication
+    def test_multiply(self):
+        """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(4, 3), 12)
         self.assertEqual(self.calc.multiply(-4, 3), -12)
         self.assertEqual(self.calc.multiply(-4, -3), 12)
-
-    def test_multiply_by_zero(self):
         self.assertEqual(self.calc.multiply(0, 5), 0)
         self.assertEqual(self.calc.multiply(5, 0), 0)
-
-    def test_multiply_large_numbers(self):
         self.assertEqual(self.calc.multiply(1000, 1000), 1_000_000)
 
-    #Division Tests
-    def test_divide_basic(self):
+    #Division
+    def test_divide(self):
+        """Test the division method."""
+        # normal division
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(9, 3), 3)
 
-    def test_divide_negative(self):
+        # negative values
         self.assertEqual(self.calc.divide(-10, 2), -5)
         self.assertEqual(self.calc.divide(10, -2), -5)
         self.assertEqual(self.calc.divide(-10, -2), 5)
 
-    def test_divide_float(self):
+        # floating point
         self.assertAlmostEqual(self.calc.divide(7, 2), 3.5)
 
-    def test_divide_by_zero(self):
-        #Return None when divide by zero
+        # division by zero
         self.assertIsNone(self.calc.divide(5, 0))
         self.assertIsNone(self.calc.divide(0, 0))
 
-    def test_divide_zero_numerator(self):
+        # zero numerator
         self.assertEqual(self.calc.divide(0, 5), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
