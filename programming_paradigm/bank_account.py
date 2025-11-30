2fe34780879aab4abf68d1fd434b759a36dbc97c
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2025-11-30 20:11:54 +0100

    changed balance to current balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,5 +13,5 @@ class BankAccount:
             self.account_balance -= amount
 
     def display_balance(self):
-        print(f"Balance ${self.account_balance}")
+        print(f" Current Balance ${self.account_balance}")
 
