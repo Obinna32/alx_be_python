8d77c696e2809e26b7d21312e89fd8dd10e8e09f
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2025-12-07 19:44:22 +0100

    changed mb to kb

--- oop/library_system.py
+++ oop/library_system.py
@@ -31,7 +31,7 @@ class Library:
         #Prints details for each book in the library
         for book in self.books:
             if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}MB")
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
                 print(f"PrintBook: {book.title} by {book.author}, Pages: {book.page_count}")
             else:
